bde413dbc14eb06eb033ebe92578abc5690776da
Wade Berrier <wberrier@gmail.com>
2024-12-26 16:02:21 -0700

    build-image: add support for building from latest snapshots
    so auto detect tar compression type

--- bin/lib/omt-common.sh
+++ bin/lib/omt-common.sh
@@ -1,5 +1,16 @@
 # shellcheck shell=bash
 
+SYSUPGRADE_EXTENSION=".bin"
+
+# This changed to .zst after 23.05
+sdk_extension() {
+	if [ "$RELEASE" = "snapshot" ] ; then
+		echo ".tar.zst"
+	else
+		echo ".tar.xz"
+	fi
+}
+
 # NOTE: must load device config first
 build_image_builder_url() {
 	if [ "$target" = "" ] || [ "$sub_target" = "" ] ; then
@@ -8,14 +19,14 @@ build_image_builder_url() {
 	fi
 
 	if [ "$RELEASE" = "snapshot" ] ; then
-		echo "https://downloads.openwrt.org/snapshots/targets/$target/$sub_target/openwrt-imagebuilder-$target-$sub_target.Linux-x86_64.tar.xz"
+		echo "https://downloads.openwrt.org/snapshots/targets/$target/$sub_target/openwrt-imagebuilder-$target-$sub_target.Linux-x86_64$(sdk_extension)"
 	else
-		echo "https://downloads.openwrt.org/releases/$RELEASE/targets/$target/$sub_target/openwrt-imagebuilder-$RELEASE-$target-$sub_target.Linux-x86_64.tar.xz"
+		echo "https://downloads.openwrt.org/releases/$RELEASE/targets/$target/$sub_target/openwrt-imagebuilder-$RELEASE-$target-$sub_target.Linux-x86_64$(sdk_extension)"
 	fi
 }
 
 build_sdk_dir() {
-	basename "$(build_image_builder_url)" .tar.xz
+	basename "$(build_image_builder_url)" "$(sdk_extension)"
 }
 
 build_archive_path() {
@@ -23,5 +34,10 @@ build_archive_path() {
 }
 
 build_image_file_path() {
-	echo "bin/targets/$target/$sub_target/openwrt-$RELEASE-$EXTRA_IMAGE_NAME-${target}-${sub_target}-${PROFILE}-squashfs-sysupgrade.bin"
+	if [ "$RELEASE" = "snapshot" ] ; then
+		image_release=""
+	else
+		image_release="-$RELEASE"
+	fi
+	echo "bin/targets/$target/$sub_target/openwrt$image_release-$EXTRA_IMAGE_NAME-${target}-${sub_target}-${PROFILE}-squashfs-sysupgrade${SYSUPGRADE_EXTENSION}"
 }
