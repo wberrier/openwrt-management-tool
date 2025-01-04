495bdb8deca2087c5ac85478c7ce3c9a6f2d7ceb
Wade Berrier <wberrier@gmail.com>
2025-01-04 08:51:27 -0700

    add scaffolding for build_image

--- src/config.rs
+++ src/config.rs
@@ -1,10 +1,12 @@
 use anyhow::Result;
 use serde::Deserialize;
+use std::path::Path;
 
-const BASE_CONFIG_FILE_NAME: &str = "conf/base.yml";
+const CONFIG_DIR: &str = "conf";
+const BASE_CONFIG_FILE_NAME: &str = "base.yml";
 
-#[derive(Deserialize)]
-pub struct BaseOptions {
+#[derive(Deserialize, Debug)]
+pub struct BaseConfig {
     pub release: String,
     pub sysupgrade_extension: String,
     pub packages: Vec<String>,
@@ -13,34 +15,43 @@ pub struct BaseOptions {
     pub disabled_services: Vec<String>,
 }
 
-#[derive(Deserialize)]
-pub struct ImageOptions {
+#[derive(Deserialize, Debug)]
+pub struct BaseIncludes {
+    pub packages: bool,
+    pub extra_packages: bool,
+    pub package_removals: bool,
+    pub disabled_services: bool,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct ImageConfig {
     pub target: String,
     pub sub_target: String,
     pub profile: String,
     pub extra_image_name: String,
-    pub packages: Vec<String>,
-    pub package_removals: Vec<String>,
+    pub packages: Option<Vec<String>>,
+    pub package_removals: Option<Vec<String>>,
     // Optional values
-    pub disabled_services: Vec<String>,
+    pub disabled_services: Option<Vec<String>>,
     pub release: Option<String>,
     pub sysupgrade_extension: Option<String>,
     // Optional include values
-    pub include_packages: bool,
-    pub include_extra_packages: bool,
-    pub include_package_removals: bool,
-    pub include_disabled_services: bool,
+    pub includes: Option<BaseIncludes>,
 }
 
-#[derive(Deserialize)]
-pub struct Options {
-    pub defaults: BaseOptions,
+#[derive(Deserialize, Debug)]
+pub struct Config {
+    pub defaults: BaseConfig,
+    pub image: ImageConfig,
 }
 
-pub fn get_options() -> Result<Options> {
-    let yaml_config = std::fs::read_to_string(BASE_CONFIG_FILE_NAME)?;
+pub fn get_config(name: String) -> Result<Config> {
+    let yaml_base_config =
+        std::fs::read_to_string(Path::new(CONFIG_DIR).join(BASE_CONFIG_FILE_NAME))?;
+    let defaults = serde_yaml::from_str(&yaml_base_config)?;
 
-    let options = serde_yaml::from_str(&yaml_config)?;
+    let yaml_image_config = std::fs::read_to_string(Path::new(CONFIG_DIR).join(name + ".yml"))?;
+    let image = serde_yaml::from_str(&yaml_image_config)?;
 
-    Ok(options)
+    Ok(Config { defaults, image })
 }
