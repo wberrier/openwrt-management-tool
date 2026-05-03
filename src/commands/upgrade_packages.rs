ada5ca60b15ddfd920b44e81ce1c4a9564afbd43
Wade Berrier <wberrier@gmail.com>
2026-05-03 12:15:53 -0600

    upgrade packages: update to use apk
    much simpler...

--- src/commands/upgrade_packages.rs
+++ src/commands/upgrade_packages.rs
@@ -5,7 +5,7 @@ use shleazy::run_shell_or_err;
 
 pub fn upgrade_packages(config: &Config) -> Result<()> {
     run_shell_or_err(format!(
-        "ssh root@{} 'opkg update && packages=$(opkg list-upgradable | awk \"{{print \\$1}}\") && if [ \"$packages\" != \"\" ] ; then opkg upgrade $packages ; else echo \"No packages to upgrade\" ; fi'",
+        "ssh root@{} 'apk update && apk upgrade'",
         &config.name
     ))
 }
